28a2e6de30b07aaf3bd139d353d3c50fd25cd22c
anupvernekar31 <43994265+anupvernekar31@users.noreply.github.com>
2026-08-13 19:04:46 +0530

    Time: 6 ms (1.47%), Space: 79.9 MB (11.47%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -5,7 +5,7 @@ class Solution {
         int sum = 0, start = 0, end = 0, s = 0;
          for(int i=0; i<nums.length;i++){
              sum+=nums[i];
-             if(max<sum){
+             if(sum > max){
                 max=sum;
                 end = i;
                 start = s;
