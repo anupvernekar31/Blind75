a010d6b99ed87b9ae588d031b0b483a7bdd75ae5
anupvernekar31 <43994265+anupvernekar31@users.noreply.github.com>
2026-08-16 21:30:52 +0530

    Time: 0 ms (100%), Space: 42.9 MB (34.22%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -14,13 +14,21 @@
  * }
  */
 class Solution {
-    public boolean isSameTree(TreeNode p, TreeNode q) {
-        if(p == null && q == null)
+    public boolean isSameTree(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
             return true;
-        if(p != null && q != null){
-            return (p.val == q.val && isSameTree(p.left,q.left) && isSameTree(p.right,q.right));
         }
-        return false;
+
+        // Case 2: If only one of the nodes is NULL, they are not identical
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+
+        // Check if the current nodes have the same data value
+        // and recursively check their left and right subtrees
+        return (node1.val == node2.val) &&
+               isSameTree(node1.left, node2.left) &&
+               isSameTree(node1.right, node2.right);
         
     }
 }
