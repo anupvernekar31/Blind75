0198cfc8b69d66eeda30fdf446ef84e507aede4b
anupvernekar31 <43994265+anupvernekar31@users.noreply.github.com>
2026-08-20 21:57:39 +0530

    Time: 15 ms (83.04%), Space: 46.8 MB (35.91%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,4 +1,8 @@
-class Solution {
+// ==========================
+// MEMOIZATION APPROACH
+// ==========================
+
+/*class Solution {
 
     int coinChangeUtil(int ind, int amount, int[][] dp, int[] coins){
         if(ind == 0){
@@ -31,5 +35,46 @@ class Solution {
         if (ans >= (int) Math.pow(10, 9))
             return -1;
         return ans;
+    }
+}*/
+
+// ==========================
+// TABULATION APPROACH
+// ==========================
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+        int n = coins.length;
+        int INF = (int) 1e9;
+        int[][] dp = new int[n][amount + 1];
+
+        // Base case: use only coins[0]
+        for (int target = 0; target <= amount; target++) {
+            if (target % coins[0] == 0) {
+                dp[0][target] = target / coins[0];
+            } else {
+                dp[0][target] = INF;
+            }
+        }
+
+        // Fill remaining rows
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 0; target <= amount; target++) {
+
+                // Do not take current coin
+                int notTake = dp[ind - 1][target];
+
+                // Take current coin (same row: unlimited use)
+                int take = INF;
+                if (coins[ind] <= target) {
+                    take = 1 + dp[ind][target - coins[ind]];
+                }
+
+                dp[ind][target] = Math.min(take, notTake);
+            }
+        }
+
+        int ans = dp[n - 1][amount];
+        return ans >= INF ? -1 : ans;
     }
 }
